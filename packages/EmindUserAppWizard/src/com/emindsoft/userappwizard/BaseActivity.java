3b8810c2d680d6e9b1276dd6dbcbcb38929a66b6
Li Bing <libing@emindsoft.com.cn>
2016-07-07 03:58:17 +0000

    EmindUserAppWizard: Firstly add
    Signed-off-by: Li Bing <libing@emindsoft.com.cn>
    Signed-off-by: Chen Gang <chengang@emindsoft.com.cn>

---
+++ packages/EmindUserAppWizard/src/com/emindsoft/userappwizard/BaseActivity.java
@@ -0,0 +1,30 @@
+package com.emindsoft.userappwizard;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewTreeObserver.OnPreDrawListener;
+
+public class BaseActivity extends Activity implements OnPreDrawListener {
+    public void onStart() {
+        super.onStart();
+        getWindow().getDecorView().setSystemUiVisibility(5638);
+        //hide the status bar
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    public void onResume() {
+        super.onResume();
+        getWindow().getDecorView().setSystemUiVisibility(5638);
+        getWindow().getDecorView().getViewTreeObserver().addOnPreDrawListener(this);
+    }
+
+    public void onPause() {
+        super.onPause();
+        getWindow().getDecorView().getViewTreeObserver().removeOnPreDrawListener(this);
+    }
+
+    public boolean onPreDraw() {
+        getWindow().getDecorView().setSystemUiVisibility(5638);
+        return true;
+    }
+}
---
+++ packages/EmindUserAppWizard/src/com/emindsoft/userappwizard/OpenthosIDRegisterActivity.java
@@ -0,0 +1,57 @@
+package com.emindsoft.userappwizard;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class OpenthosIDRegisterActivity extends BaseActivity {
+    private Button mButtonFinish;
+    private Button mButtonCancel;
+    private WebView mWebView;
+    private  final String URL="http://dev.openthos.org/?q=user/register";
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_openthos_id_register);
+
+        this.mWebView = (WebView) findViewById(R.id.webview_register);
+        this.mButtonFinish = (Button) findViewById(R.id.button_register_finish);
+        this.mButtonCancel = (Button) findViewById(R.id.button_cancel);
+
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl(URL);
+
+        //mWebView.setWebViewClient(new WebViewClient(){
+          //  @Override
+            //public boolean shouldOverrideUrlLoading(WebView view, String url) {
+              //  view.loadUrl(url);
+                //return true;
+       //     }
+       // });
+
+        this.mButtonFinish.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString("opethosid","xxxx@xx.com");
+                Intent mIntent = new Intent();
+                mIntent.putExtras(bundle);
+                setResult(RESULT_OK, mIntent);
+                finish();
+            }
+        });
+        this.mButtonCancel.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                Intent mIntent1 = new Intent();
+                mIntent1.putExtras(bundle);
+                setResult(RESULT_CANCELED, mIntent1);
+                finish();
+            }
+        });
+    }
+}
