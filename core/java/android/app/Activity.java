ab5ab7eb3d0d12eccf7629b6b5fa0e213410d993
Li Bing <libing@emindsoft.com.cn>
2016-12-06 12:46:52 +0000

    frameworks: base: Support total fullscrren to microsoft office powerpoint
    At present, status bar is always auto-hide, so status bar can be hidden
    when switching focus. But we still need notice about it, and fix it next.
    
    Signed-off-by: Li Bing <libing@emindsoft.com.cn>
    Signed-off-by: Chen Gang <chengang@emindsoft.com.cn>

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -4019,6 +4019,12 @@ public class Activity extends ContextThemeWrapper
     @Override
     public void startActivity(Intent intent, @Nullable Bundle options) {
         if (options != null) {
+            if (getPackageName().equals(ApplicationInfo.APPNAME_OFFICE_POWERPOINT)) {
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_FULLSCREEN);
+                Intent intent1 = new Intent();
+                intent1.setAction(Intent.STATUS_BAR_HIDE);
+                this.sendBroadcast(intent1);
+            }
             startActivityForResult(intent, -1, options);
         } else {
             // Note we want to go through this call for compatibility with
